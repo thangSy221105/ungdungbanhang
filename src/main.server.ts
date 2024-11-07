199a041c3998472f690de2b5f91ee385a8b9b369
SyThang <vst221105@gmail.com>
2024-11-07 23:28:26 +0700

    init project

---
+++ src/main.server.ts
@@ -0,0 +1,4 @@
+import { bootstrapApplication } from '@angular/platform-browser'; 
+import { HomeComponent } from './app/home/home/home.component'; 
+import { config } from './app/app.config.server'; 
+const bootstrap = () => bootstrapApplication(HomeComponent, config); export default bootstrap;
